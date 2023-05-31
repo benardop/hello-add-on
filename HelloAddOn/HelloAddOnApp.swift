8afb58278547946d51071988e613ca5c790e650d
Benard <benard.oc.p@gmail.com>
2023-05-31 22:23:45 +0200

    Initial Commit

---
+++ HelloAddOn/HelloAddOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAddOnApp.swift
+//  HelloAddOn
+//
+//  Created by user241871 on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloAddOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
