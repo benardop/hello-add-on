55b7d68e7596422b19dffbe2746773a13c174da3
Benard <benard.oc.p@gmail.com>
2023-06-08 10:04:23 +0200

    Initail commit

--- HelloAddOn/ContentView.swift
+++ HelloAddOn/ContentView.swift
@@ -2,18 +2,32 @@
 //  ContentView.swift
 //  HelloAddOn
 //
-//  Created by user241871 on 5/31/23.
+//  Created by user241871 on 5/29/23.
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var isShowingWebView = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if isShowingWebView {
+                WebView(urlString: Bundle.main.path(forResource: "MyOffice", ofType: "html")!)
+                    .frame(height: 300)
+            }
+            
+            Button(action: {
+                isShowingWebView.toggle()
+            }) {
+                Text(isShowingWebView ? "Hide Web View" : "Show Web View")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
@@ -22,5 +36,20 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let urlString: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let url = URL(string: urlString) else { return }
+        let request = URLRequest(url: url)
+        uiView.load(request)
     }
 }
